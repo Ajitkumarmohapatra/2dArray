4d4a34792ddb60b3a2f619064623c190d79cee88
Ajit kumar mohapatra <167737649+Ajitkumarmohapatra@users.noreply.github.com>
2024-05-30 20:23:35 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class test{
+    public static void main(String[]args)
+    {
+        int arr[][] =new int[2][3];
+       
+        arr[0][0]=10;
+        arr[0][1]=11;
+        arr[0][2]=13;
+        arr[1][0]=10;
+         System.out.println(arr[1][2]);
+
+    }
+}
